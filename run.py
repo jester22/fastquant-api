142e2e1008e6e60a89a550445029525bc1fe969b
Jester Temporada <jester.temporada@gmail.com>
2020-05-10 23:17:41 +0800

    Add run script

---
+++ run.py
@@ -0,0 +1,7 @@
+import uvicorn
+
+async def app(scope, receive, send):
+    ...
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload="true")
